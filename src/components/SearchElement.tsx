8f1b155273922160c61728526b1bad6fa98c234e
Adam <delsarte.adam@gmail.com>
2024-09-27 15:45:13 +0200

    new search bar

--- src/components/SearchElement.tsx
+++ src/components/SearchElement.tsx
@@ -1,5 +1,5 @@
 //import styles from "./SearchElement.module.scss";
-import { useRef, useEffect, useState } from "react";
+import { useState } from "react";
 
 type props = {
   height: string;
@@ -16,81 +16,105 @@ function SearchElement({
   searchIconSize,
   maxWidth,
 }: props) {
-  const searchDiv = useRef<HTMLDivElement>(null);
-  const [radiusBorder, setRadiusBorder] = useState(0);
-  const [click, setClick] = useState(false);
+  // const [click, setClick] = useState(false);
+  const [focus, setFocus] = useState<React.CSSProperties>({});
+  const [focusWithin, setFocusWithin] = useState<React.CSSProperties>({});
 
   // useEffect hook to handle after the component renders
-  useEffect(() => {
-    if (searchDiv.current) {
-      const height = searchDiv.current.offsetHeight;
-      setRadiusBorder(height / 2);
-    }
-  }, [searchDiv]);
 
-  function handleClick() {
-    setClick(!click);
-  }
+  // function handleClick() {
+  //   setClick(!click);
+  // }
+
+  const handleFocus = () => {
+    setFocus({
+      outline: 0,
+    });
+    setFocusWithin({
+      width: "100%",
+    });
+  };
+
+  const handleBlur = () => {
+    setFocus({});
+    setFocusWithin({});
+  };
 
   return (
-    <div style={{ width: "100%", maxWidth: maxWidth }}>
-      <div
+    <>
+      <div //box
         style={{
-          height: height,
+          background: color,
+          width: maxWidth,
+          padding: "3em",
+          boxShadow: "0 0 3em rgba(0, 0, 0, 0.15)",
           position: "relative",
-          maxWidth: !click ? height : "95%",
-          backgroundColor: color,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: `${radiusBorder}px`,
-          margin: " auto",
         }}
-        ref={searchDiv}
       >
-        {click ? (
+        <div //search-bar
+          style={{
+            border: "1px solid red",
+            display: "flex",
+            borderRadius: `calc(${height} / 2)`,
+            height: height,
+            padding: `calc(${height} * 0.05)`,
+            position: "relative",
+            width: height, // = height if close
+            transition: "width 300ms ease-in-out",
+            ...focusWithin,
+          }}
+        >
           <input
             type="text"
             placeholder="Search..."
+            aria-label="search"
             style={{
-              height: "100%",
-              width: "100%",
-              position: "absolute",
-              borderRadius: `${radiusBorder}px`,
-              background: color,
-              outline: "none",
+              flexGrow: 1,
+              fontSize: `calc(${height} / 2 * 0.8)`,
+              padding: "0 0.5em",
               border: "none",
-              paddingLeft: `${radiusBorder / 2}px`,
-              fontSize: `${Math.ceil(radiusBorder * 0.8)}px`,
+              background: "red",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              lineHeight: `calc(${height} * 0.05)`, // for all navigator
+              ...focus,
             }}
+            onFocus={() => handleFocus()}
+            onBlur={() => handleBlur()}
           />
-        ) : (
-          ""
-        )}
-
-        <svg
-          id="Layer_1"
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox="0 0 512 512"
-          preserveAspectRatio="xMidYMid meet" // Keep aspect ratio centered
-          style={{
-            height: `${searchIconSize}%`,
-            aspectRatio: "1/1",
-            position: "absolute",
-            right: !click ? "" : `${radiusBorder / 2}px`,
-          }}
-          onClick={() => handleClick()}
-        >
-          <title>search</title>
-          <path
-            d="M503.4,438.34l-152-152.21a188.91,188.91,0,0,0,26.81-95.48C378.89,86.09,294.84.75,190.4,0S.75,83.43,0,188,83.3,377.91,187.72,378.65a187.94,187.94,0,0,0,98.83-27.34L438.44,503.39a29.63,29.63,0,0,0,41.77,0l23.2-23.21A29.7,29.7,0,0,0,503.4,438.34ZM188,335.3c-80.54-.55-145.34-66.37-144.76-147s66.3-145.53,146.84-145,145.33,66.38,144.76,147A146.07,146.07,0,0,1,188,335.3Z"
-            fill={fill}
-          />
-        </svg>
+          <button
+            style={{
+              cursor: "pointer",
+              height: `calc(${height} * 0.90)`,
+              width: `calc(${height} * 0.90)`,
+              borderRadius: `calc(${height} / 2)`,
+              border: "none",
+            }}
+          >
+            <svg
+              id="Layer_1"
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 512 512"
+              preserveAspectRatio="xMidYMid meet" // Keep aspect ratio centered
+              style={{
+                height: `${searchIconSize}%`,
+                aspectRatio: "1/1",
+              }}
+            >
+              <title>search</title>
+              <path
+                d="M503.4,438.34l-152-152.21a188.91,188.91,0,0,0,26.81-95.48C378.89,86.09,294.84.75,190.4,0S.75,83.43,0,188,83.3,377.91,187.72,378.65a187.94,187.94,0,0,0,98.83-27.34L438.44,503.39a29.63,29.63,0,0,0,41.77,0l23.2-23.21A29.7,29.7,0,0,0,503.4,438.34ZM188,335.3c-80.54-.55-145.34-66.37-144.76-147s66.3-145.53,146.84-145,145.33,66.38,144.76,147A146.07,146.07,0,0,1,188,335.3Z"
+                fill={fill}
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
