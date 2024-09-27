9f1c219bc692efded4a90b630e80ff93fcc9d7f4
Adam <delsarte.adam@gmail.com>
2024-09-27 17:36:39 +0200

    absolute added

--- src/components/SearchElement.tsx
+++ src/components/SearchElement.tsx
@@ -52,80 +52,83 @@ function SearchElement({
 
   return (
     <>
-      <div //box
-        style={{
-          background: color,
-          width: maxWidth,
-          padding: "3em",
-          boxShadow: "0 0 3em rgba(0, 0, 0, 0.15)",
-          position: "relative",
-        }}
+      <div
+        /* where the box is */ style={{ position: "relative", width: height }}
       >
-        <div //search-bar
-          style={{
-            border: "1px solid red",
-            display: "flex",
-
-            borderRadius: `calc(${height} / 2)`,
-            height: height,
-            padding: `calc(${height} * 0.05)`,
-            position: "relative",
-            width: height, // = height if close
-            transition: "width 300ms ease-in-out",
-            overflow: "hidden",
-            ...focusWithin,
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Search..."
-            aria-label="search"
+        <div /*absolute for the searchBar */ style={{ position: "absolute" }}>
+          <div /*box*/
             style={{
-              flexGrow: 1,
-              fontSize: `calc(${height} / 2 * 0.8)`,
-              padding: "0 0.5em",
-              border: "none",
-              position: "absolute",
-              top: 0,
-              bottom: 0,
-              ...leftInput,
-              opacity: 0,
-              lineHeight: `calc(${height} * 0.05)`, // for all navigator
-              cursor: "pointer",
-              ...focus,
-            }}
-            onFocus={() => handleFocus()}
-            onBlur={() => handleBlur()}
-          />
-          <button
-            style={{
-              cursor: "pointer",
-              height: `calc(${height} * 0.90)`,
-              width: `calc(${height} * 0.90)`,
-              borderRadius: `calc(${height} / 2)`,
-              border: "none",
-              ...leftbutton,
+              width: maxWidth,
+              position: "relative",
             }}
           >
-            <svg
-              id="Layer_1"
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 512 512"
-              preserveAspectRatio="xMidYMid meet" // Keep aspect ratio centered
+            <div /*search-bar*/
               style={{
-                height: `${searchIconSize}%`,
-                aspectRatio: "1/1",
+                display: "flex",
+                background: color,
+                borderRadius: `calc(${height} / 2)`,
+                height: height,
+                padding: `calc(${height} * 0.05)`,
+                position: "relative",
+                width: height, // = height if close
+                transition: "width 300ms ease-in-out",
+                overflow: "hidden",
+                ...focusWithin,
               }}
             >
-              <title>search</title>
-              <path
-                d="M503.4,438.34l-152-152.21a188.91,188.91,0,0,0,26.81-95.48C378.89,86.09,294.84.75,190.4,0S.75,83.43,0,188,83.3,377.91,187.72,378.65a187.94,187.94,0,0,0,98.83-27.34L438.44,503.39a29.63,29.63,0,0,0,41.77,0l23.2-23.21A29.7,29.7,0,0,0,503.4,438.34ZM188,335.3c-80.54-.55-145.34-66.37-144.76-147s66.3-145.53,146.84-145,145.33,66.38,144.76,147A146.07,146.07,0,0,1,188,335.3Z"
-                fill={fill}
+              <input
+                type="text"
+                placeholder="Search..."
+                aria-label="search"
+                style={{
+                  flexGrow: 1,
+                  fontSize: `calc(${height} / 2 * 0.8)`,
+                  padding: "0 0.5em",
+                  border: "none",
+                  background: color,
+                  position: "absolute",
+                  top: 0,
+                  bottom: 0,
+                  ...leftInput,
+                  opacity: 0,
+                  lineHeight: `calc(${height} * 0.05)`, // for all navigator
+                  cursor: "pointer",
+                  ...focus,
+                }}
+                onFocus={() => handleFocus()}
+                onBlur={() => handleBlur()}
               />
-            </svg>
-          </button>
+              <button
+                style={{
+                  cursor: "pointer",
+                  height: `calc(${height} * 0.90)`,
+                  width: `calc(${height} * 0.90)`,
+                  borderRadius: `calc(${height} / 2)`,
+                  border: "none",
+                  ...leftbutton,
+                }}
+              >
+                <svg
+                  id="Layer_1"
+                  data-name="Layer 1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 512 512"
+                  preserveAspectRatio="xMidYMid meet" // Keep aspect ratio centered
+                  style={{
+                    height: `${searchIconSize}%`,
+                    aspectRatio: "1/1",
+                  }}
+                >
+                  <title>search</title>
+                  <path
+                    d="M503.4,438.34l-152-152.21a188.91,188.91,0,0,0,26.81-95.48C378.89,86.09,294.84.75,190.4,0S.75,83.43,0,188,83.3,377.91,187.72,378.65a187.94,187.94,0,0,0,98.83-27.34L438.44,503.39a29.63,29.63,0,0,0,41.77,0l23.2-23.21A29.7,29.7,0,0,0,503.4,438.34ZM188,335.3c-80.54-.55-145.34-66.37-144.76-147s66.3-145.53,146.84-145,145.33,66.38,144.76,147A146.07,146.07,0,0,1,188,335.3Z"
+                    fill={fill}
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -4,10 +4,11 @@ import styles from "./Test.module.scss";
 function Test() {
   return (
     <div className={styles.container}>
+      <h1>test</h1>
       <SearchElement
         height={"10vh"}
-        color={"#fff"}
-        fill={"#f0c345"}
+        color={"#f0c345"}
+        fill={"#c0c0c0"}
         searchIconSize={60}
         maxWidth={"1000px"}
         leftPosition={false}
